a173cd29ffeeb2d2b140da4baa190fa771ce9f3a
Lalodian <angeldaysi2003@gmail.com>
2023-03-29 19:27:10 -0600

    Angel

---
+++ Lalo_Antonio_V2/Assets/Videojuego/Scrips jugador/Bala.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    [SerializeField] private float velocidad;
+    [SerializeField] private float daño;
+    [SerializeField] private float tiempoDeDestruir;
+
+    void Start()
+    {
+        // Destruye el objeto después de 3 segundos
+        Invoke("DestruirObjeto", tiempoDeDestruir);
+    }
+
+    void DestruirObjeto()
+    {
+        // Destruye el objeto
+        Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector2.right * velocidad * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Colisión detectada con objeto de etiqueta: " + collision.gameObject.tag);
+
+        if (collision.gameObject.CompareTag("Enemigo"))
+        {
+            collision.GetComponent<Enemigo>().TomarDaño(daño);
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Lalo_Antonio_V2/Assets/Videojuego/Scrips jugador/Enemigo.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    [SerializeField] private float vida;
+    [SerializeField] private GameObject efectoMuerte;
+
+    public void TomarDaño(float daño)
+    {
+        vida -= daño;
+        if(vida <=0)
+        {
+            Muerte();
+        }
+    }
+    public void Muerte()
+    {
+        Instantiate(efectoMuerte, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameManager.Instance.PerderVida();
+            Destroy(this.gameObject);
+
+        }
+    }
+}
